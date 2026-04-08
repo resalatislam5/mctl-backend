dada8ddc13a28a896de3275e5c57b57d8480c46f
Resalat Islam <resalatislam5@gmail.com>
2026-04-08 21:31:34 +0600

    implement check permission in report

--- src/routes/auditLogRoutes.ts
+++ src/routes/auditLogRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import auditLogController from '../api/v1/auditLog/auditLog.controller';
 import { authenticate } from '../middleware/authenticate';
+import { checkPermissionMiddleware } from '../middleware/check.permission.middleware';
 
 const auditLogRoutes: Router = Router().get(
   '/',
   authenticate,
+  checkPermissionMiddleware('AUDIT', 'can_read'),
   auditLogController.findAll,
 );
 
--- src/routes/reportRoutes.ts
+++ src/routes/reportRoutes.ts
@@ -2,15 +2,32 @@ import { Router } from 'express';
 import accountTransactionController from '../api/v1/accountTransaction/accountTransaction.controller';
 import reportController from '../api/v1/Report/report.controller';
 import { authenticate } from '../middleware/authenticate';
+import { checkPermissionMiddleware } from '../middleware/check.permission.middleware';
 
 const ReportRoutes: Router = Router()
-  .get('/student-ledger', authenticate, reportController.studentLedger)
-  .get('/expense', authenticate, reportController.expenseReport)
+  .get(
+    '/student-ledger',
+    authenticate,
+    checkPermissionMiddleware('STUDENT_LEDGER', 'can_read'),
+    reportController.studentLedger,
+  )
+  .get(
+    '/expense',
+    authenticate,
+    checkPermissionMiddleware('EXPENSE_HISTORY', 'can_read'),
+    reportController.expenseReport,
+  )
   .get(
     '/upcoming-installment',
     authenticate,
+    checkPermissionMiddleware('UPCOMING_INSTALLMENT', 'can_read'),
     reportController.upcomingInstallments,
   )
-  .get('/account-ledger', authenticate, accountTransactionController.findAll);
+  .get(
+    '/account-ledger',
+    authenticate,
+    checkPermissionMiddleware('ACCOUNT_LEDGER', 'can_read'),
+    accountTransactionController.findAll,
+  );
 
 export default ReportRoutes;
